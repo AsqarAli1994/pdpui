51350833e5998f29905c810cea8a47a4669cc9e1
olim <asqarali.olimov@gmail.com>
2022-06-30 10:51:47 +0500

    - initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title: Center(child: Text("UI")),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text("User", style: TextStyle(fontSize: 18, color: Colors.red),),
+            ),
+            SizedBox(
+              width: 6,
+            ),
+            Center(
+              child: Text("Interface", style: TextStyle(fontSize: 18, color: Colors.green),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
